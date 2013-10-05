522a182bf563842f67812357ee1c7227b7b9f50a
Adam Cardeans <adam.cardenas@colorado.edu>
2013-10-05 15:39:28 -0600

    Removed properties code

--- src/main/java/gears/Driver.java
+++ src/main/java/gears/Driver.java
@@ -1,8 +1,5 @@
 package gears;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Properties;
 
 public class Driver {
 
@@ -13,21 +10,6 @@ public class Driver {
 		
 		System.out.println(String.format("Gots it working, %s",System.getProperty("java.version")));
 		
-		Properties prop = new Properties();
-		try {
-    		//set the properties value
-    		prop.setProperty("database", "localhost");
-    		prop.setProperty("dbuser", "mkyong");
-    		prop.setProperty("dbpassword", "newPassword");
- 
-    		//save properties to project root folder
-    		
-    		prop.store(new FileOutputStream(Constant.CONFIG_PATH), null);
-    		System.out.println("Stored properties");
- 
-    	} catch (IOException ex) {
-    		ex.printStackTrace();
-        }
 		
 		
 	}
