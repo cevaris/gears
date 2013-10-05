fc8577bd6c51ef9081c427d81818468003e151a6
James Kobol <jim.kobol@gmail.com>
2013-10-05 15:18:16 -0600

    added maven project

---
+++ src/main/java/com/gears/java/Driver.java
@@ -0,0 +1,14 @@
+package com.gears.java;
+
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(String.format("Gots it working, %s",System.getProperty("java.version")));
+
+	}
+
+}
