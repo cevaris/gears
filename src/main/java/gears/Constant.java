d07b31cf2a8a0037842061541dc854ab6477fbcf
James Kobol <jim.kobol@gmail.com>
2013-10-05 15:37:51 -0600

    added properties file; added aws tools via maven

---
+++ src/main/java/gears/Constant.java
@@ -0,0 +1,8 @@
+package gears;
+
+public class Constant {
+	
+	public static String RESOURCES = "src/main/resources/";
+	public static String CONFIG_PATH = RESOURCES + "config.properties";
+
+}
--- src/main/java/gears/Driver.java
+++ src/main/java/gears/Driver.java
@@ -1,14 +1,36 @@
 package gears;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Driver {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println(String.format("Gots it working, %s",System.getProperty("java.version")));
-
+		
+		Properties prop = new Properties();
+		try {
+    		//set the properties value
+    		prop.setProperty("database", "localhost");
+    		prop.setProperty("dbuser", "mkyong");
+    		prop.setProperty("dbpassword", "newPassword");
+ 
+    		//save properties to project root folder
+    		
+    		prop.store(new FileOutputStream(Constant.CONFIG_PATH), null);
+    		System.out.println("Stored properties");
+ 
+    	} catch (IOException ex) {
+    		ex.printStackTrace();
+        }
+		
+		
 	}
 
 }
+	
