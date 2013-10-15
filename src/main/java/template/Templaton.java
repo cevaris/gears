644626b331ce5717fee48f6b6d81f305d6a0012c
Cevaris <cevaris@gmail.com>
2013-10-15 15:19:05 -0600

    created template class; created test for template rendering.

--- src/main/java/template/Templaton.java
+++ src/main/java/template/Templaton.java
@@ -1,5 +1,49 @@
 package template;
 
+import java.io.StringWriter;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.context.Context;
+
 public class Templaton {
+	
+	
+	VelocityContext context = null;
+	StringWriter writer = null;
+	Template template = null;
+	boolean hasRendered = false;
+	
+	public Templaton(String templatePath) {
+		init(templatePath, null);
+	}
+	
+	public Templaton(String templateName, StringWriter writer) {
+		init(templateName, writer);
+	}
+	
+	public void init(String templatePath, StringWriter writer){
+		this.context = new VelocityContext();
+		this.template = Velocity.getTemplate(templatePath);
+		this.writer = writer;
+		
+		this.hasRendered = false;
+	}
+	
+	public Context put(String key, Object value) {
+		this.context.put(key,value);
+		return this.context;
+	}
+	
+	public StringWriter render() {
+		if(this.hasRendered) return this.writer;
+		if(this.writer == null) this.writer = new StringWriter();
+		
+		this.template.merge(context, writer);
+		this.hasRendered = true;
+		
+		return this.writer;
+	}
 
 }
--- src/test/java/com/template/TemplateTest.java
+++ src/test/java/com/template/TemplateTest.java
@@ -2,37 +2,28 @@ package com.template;
 
 import static org.junit.Assert.*;
 
-import java.io.StringWriter;
-
-import org.apache.velocity.Template;
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.Velocity;
 import org.junit.Test;
+
+import template.Templaton;
 
 public class TemplateTest {
 	
 	public static String TEST_RESOURCES = "src/test/java/resources/";
 	public static String CONFIG_NGINX = TEST_RESOURCES + "configNginx.vm";
 
+	
+	
 	@Test
-	public void testConfigNginx() {
+	public void testTemplatonNginx() {
 		
-		VelocityContext context = new VelocityContext();
-		Template template = null;
-		
-		System.out.println("Working Directory = " +
-	              System.getProperty("user.dir"));
+		Templaton template = new Templaton(CONFIG_NGINX);
 		
         try {
-            context.put("port",  "80");
-            context.put("webName", "example.com" );
-            context.put("projectPath", "/var/www/my_awesome_rails_app/public");
-            template = Velocity.getTemplate(CONFIG_NGINX);
-            assertNotNull(template.getData());
-            
-            StringWriter writer = new StringWriter();
-            template.merge(context, writer);
-            System.out.println(writer);
+        	template.put("port",  "8888");
+        	template.put("webName", "example.com" );
+        	template.put("projectPath", "/var/www/my_awesome_rails_app/public");
+            assertTrue(template.render().toString().length() > 0);
+            System.out.println(template.render());
         } catch( Exception e ) {
           System.err.println("Exception caught: " + e.getMessage());
         }
