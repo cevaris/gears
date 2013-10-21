cb00bb8bdb854a18c573180cc6f48e7a2cfccbfc
Cevaris <cevaris@gmail.com>
2013-10-20 23:03:36 -0600

    have working LAMP stack test case breaking down the install using three different apps; Apache, MySQL, and PHP

--- src/test/java/gears/LAMPStackTest2.java
+++ src/test/java/gears/LAMPStackTest2.java
@@ -14,14 +14,69 @@ import base.Instance;
 import base.Server;
 import base.ServerConfiguration;
 
-public class MySQLTest {
+public class LAMPStackTest2 {
 
 	public static String TEST_RESOURCES = "src/test/java/resources/";
 	public static String HOSTS = TEST_RESOURCES + "hosts.yaml";
 	public static String INFO = TEST_RESOURCES + "info.php.vm";
 	
+	
+	class PHPApp extends Application {
+		
+		public PHPApp(Server server) {
+			super(server);
+		}
+
+		@Override
+		protected void execute() {
+			// Update application repository
+			update();
+
+			// Install misc apps
+			install(new String[]{"php5"}, new String[]{"-q","-y"});
+		}
+		    
+	}
+	
+	class ApacheApp extends Application {
+		
+		String MYSQL_PASS   = "mypass";
+		String MYSQL_USER   = "root";
+		
+		public ApacheApp(Server server) {
+			super(server);
+		}
+
+		private void renderInfo(){
+			VelocityContext context = Templaton.getContext();
+        	context.put("MYSQL_PASS", MYSQL_PASS);
+        	context.put("MYSQL_USER", MYSQL_USER );
+			render(INFO, "/var/www/info.php", context);
+		}
+
+		@Override
+		protected void execute() {
+			// Update application repository
+			update();
+			
+			// Install misc apps
+			install(new String[]{"apache2", "libapache2-mod-php5", "php5-mcrypt"}, 
+					new String[]{"-q","-y"});
+	
+			renderInfo();
+			
+			// Restart Apache service, equals to "service apache2 restart"
+			restart("apache2");
+		}
+		    
+	}
+	
 	class MySQLApp extends Application {
 		
+		/**
+		 * For MySQL config file
+		 * http://stackoverflow.com/questions/1167056/optimal-mysql-configuration-my-cnf
+		 */
 		String MYSQL_PASS   = "mypass";
 		String MYSQL_USER   = "root";
 		String INNODB_BUFFER_POOL_SIZE = "512M";
@@ -54,22 +109,25 @@ public class MySQLTest {
 		    
 	}
 	
-	class DBServer extends Server {
+	class LAMPStackServer extends Server {
 		
-		class ProductionMySQL extends ServerConfiguration {
-			public ProductionMySQL() {
-				Instance dbServer = new Instance();
-				dbServer.setFQDN("192.168.1.101");
-				dbServer.setSSHPermKeyPath("/Users/cevaris/.ssh/id_rsa");
-				addInstance(dbServer);
+		class ProductionLAMP extends ServerConfiguration {
+			public ProductionLAMP() {
+				Instance server = new Instance();
+				server.setFQDN("192.168.1.102");
+				server.setSSHPermKeyPath("/Users/cevaris/.ssh/id_rsa");
+				addInstance(server);
 			}
 		}
 		
-		public DBServer() {
-			this.config = new ProductionMySQL();
+		public LAMPStackServer() {
+			this.config = new ProductionLAMP();
 			
+			this.applications.add(new PHPApp(this));
 			this.applications.add(new MySQLApp(this));
-			this.execute();
+			this.applications.add(new ApacheApp(this));
+			
+			execute();
 		}
 
 	}
@@ -82,8 +140,8 @@ public class MySQLTest {
 //	}
 	
 	@Test
-	public void testDBServer(){
-		Server server   = new DBServer();
+	public void testLAMPServer(){
+		Server server   = new LAMPStackServer();
 //		Application app = new MySQLApp(server);
 //		assertNotNull(apache.getSession());
 	}
