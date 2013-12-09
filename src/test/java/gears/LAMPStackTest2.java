a7c4e2ec131f45b0f44ecd901d684bf61c58d870
Cevaris <cevaris@gmail.com>
2013-12-08 17:44:50 -0700

    got working stuffs

--- src/test/java/gears/LAMPStackTest2.java
+++ src/test/java/gears/LAMPStackTest2.java
@@ -96,20 +96,6 @@ public class LAMPStackTest2 {
 
 	}
 	
-	class MySQLClientApp extends Gear {
-
-		@Override
-		public void execute() {
-			update();
-			
-			// Install misc apps
-			install( "-y", "mysql-client php5-mysql" );
-			
-		}
-
-	}
-	
-	
 	class ProductionLAMP {
 		
 		private final static String SSH_KEY = "/Users/cevaris/Documents/workspace/gears/gears/keys/id_rsa";
@@ -119,9 +105,14 @@ public class LAMPStackTest2 {
 		public ProductionLAMP() {
 			Instance instance1 = new Instance("192.168.2.100", SSH_KEY);
 			config.addInstance("web", instance1);
+			Instance instance2 = new Instance("192.168.2.102", SSH_KEY);
+			config.addInstance("web", instance2);
 			
-			Instance instance2 = new Instance("192.168.2.101", SSH_KEY);
-			config.addInstance("db", instance2);
+			Instance instance3 = new Instance("192.168.2.101", SSH_KEY);
+			config.addInstance("db", instance3);
+			
+//			Instance instance4 = new Instance("192.168.2.103", SSH_KEY);
+//			config.addInstance("lb", instance4);
 		}
 	}
 	
@@ -130,7 +121,6 @@ public class LAMPStackTest2 {
 		
 		Gear vim    = new VimApp();
 		Gear mysqlServer = new MySQLServerApp();
-		Gear mysqlClient = new MySQLClientApp();
 		Gear php    = new PHPApp();
 		Gear apache = new ApacheApp();
 		
